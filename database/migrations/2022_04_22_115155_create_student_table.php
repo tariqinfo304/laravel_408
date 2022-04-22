8f112c662c967499e45657c94c0dc2fb87fae3aa
Muhammad Tariq <53593504+tariqinfo304@users.noreply.github.com>
2022-04-22 17:13:19 +0500

    Migration + Alter Schem Migratin

---
+++ database/migrations/2022_04_22_115155_create_student_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student', function (Blueprint $table) {
+            $table->id();
+            $table->string("name")->nullable();
+            $table->integer("age");
+            $table->string("cnic",15);
+            $table->float("height");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student');
+    }
+};
